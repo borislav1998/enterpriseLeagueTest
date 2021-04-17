22ad67dd71a64d04c0ac7b4c62651f92e97fdc3c
borislav1998 <borislav.manevski98@gmail.com>
2021-04-17 16:01:03 +0200

    final

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
-console.log("Boro e najjak");
+
+import $ from "jquery";
+var location;
+$.getJSON("https://api.db-ip.com/v2/free/self", function(data) {
+  location=data;
+  if(location.continentCode == "EU" && location.countryCode == "UK"){
+    $("#businessPrice").html("£10.99<span class=\"period\">/month</span>");
+    $("#premiumPrice").html("£4.99<span class=\"period\">/month</span>");
+  }else if(location.continentCode == "EU"){
+    $("#businessPrice").html("€10.99<span class=\"period\">/month</span>");
+    $("#premiumPrice").html("€4.99<span class=\"period\">/month</span>");
+  }else{
+    $("#businessPrice").html("$10.99<span class=\"period\">/month</span>");
+    $("#premiumPrice").html("$4.99<span class=\"period\">/month</span>");
+  }
+});
+
